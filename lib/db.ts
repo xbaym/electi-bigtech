0445c7042fb52096df885ef0b79a73e54687d686
muha <m.orazbay@astanait.edu.kz>
2026-01-15 20:04:34 -0800

    update

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,16 @@
+// lib/db.ts
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
+const globalForPrisma = global as unknown as {
     prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: ['error'],
+    })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma
+}
